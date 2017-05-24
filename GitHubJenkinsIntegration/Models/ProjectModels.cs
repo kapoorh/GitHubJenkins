e82172184f9853a227fb9a247e285e308c226ce7
kapoorh <harsh.kapoor@birlasoft.com>
2017-05-24 07:07:42 +0530

    Code Checkin

---
+++ GitHubJenkinsIntegration/Models/ProjectModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GitHubJenkinsIntegration.Models
+{
+    public class ProjectModels
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string ProjectName
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string Location
+        {
+            get;
+            set;
+        }
+        public bool IsEdit
+        {
+            get;
+            set;
+        }
+    }
+}
