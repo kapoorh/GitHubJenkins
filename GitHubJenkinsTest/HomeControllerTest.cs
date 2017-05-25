3168b03e7cd5cf0276bd62e38a95b30708d73fe9
kapoorh <harsh.kapoor@birlasoft.com>
2017-05-25 18:37:10 +0530

    commit unit test cases

--- GitHubJenkinsTest/HomeControllerTest.cs
+++ GitHubJenkinsTest/HomeControllerTest.cs
@@ -1,16 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using NUnit.Framework;
 
 namespace GitHubJenkinsTest
 {
-    [TestClass]
+    [TestFixture]
     public class HomeControllerTest
     {
-        [TestMethod]
+        [Test]
         public void SampleTest()
         {
             Assert.AreEqual("HomeController", "HomeController");
